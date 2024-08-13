34e48dec632e2355fab2e688a7699f756075563c
Lucas Costa <lucascostafilho@hotmail.com>
2024-08-13 01:28:22 -0400

    Criando páginas com handlebars

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname + '/index.html'));
+const {engine} = require("express-handlebars");
+
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    },
+
+}))
+
+app.set('view engine', 'handlebars');
+router.get('/', function(req, res){
+    res.render('home');
 })
-router.get('/about',function(req,res){
+router.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
 })
 
